a1fa5da5e2a40b8be2bfe274627e24425de79a29
George Marselis <gmarselis@vetinst.no>
2026-04-16 14:33:16 +0200

    feat reporting_functions.sh: add report generation for sorting by organism

--- reporting_functions.sh
+++ reporting_functions.sh
@@ -1,1 +1,3 @@
 get_irida_sample_counts --sort-by-samples | python3 -c "import sys,pandas as pd,matplotlib.pyplot as plt; df=pd.read_fwf(sys.stdin); df=df.iloc[::-1]; df.plot.barh(x='project',y='samples',figsize=(10,20),legend=False); plt.tight_layout(); plt.savefig('/tmp/irida_samples.png',dpi=300)"
+
+get_irida_samples_by_organism --sort-by-organism --group | python3 -c "import sys,pandas as pd,matplotlib.pyplot as plt; df=pd.read_fwf(sys.stdin); df=df.iloc[::-1]; df.plot.barh(x='organism',y='total_samples',figsize=(10,20),legend=False); plt.tight_layout(); plt.savefig('/tmp/irida_by_organism.png',dpi=300)"
