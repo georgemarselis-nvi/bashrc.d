618c6e80c666b837a9264863690f1797eb36d548
George Marselis <gmarselis@vetinst.no>
2026-04-16 14:15:00 +0200

    feat add get_irida_samples_by_organism: get all the samples and sort them with --sort-by-samples, --sort-by-organism.  --group flag groups by sample and organism respectively.

---
+++ reporting_functions.sh
@@ -0,0 +1,1 @@
+get_irida_sample_counts --sort-by-samples | python3 -c "import sys,pandas as pd,matplotlib.pyplot as plt; df=pd.read_fwf(sys.stdin); df=df.iloc[::-1]; df.plot.barh(x='project',y='samples',figsize=(10,20),legend=False); plt.tight_layout(); plt.savefig('/tmp/irida_samples.png',dpi=300)"
